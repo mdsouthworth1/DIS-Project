fc7029be3b0c46d08e7a5982a6f5c58acc99d3a0
Shailendra Singh <shailendrasingh@usf.edu>
2022-11-28 16:33:23 -0500

    fix: static file path issue
    - Moved all static files in data folder
    - Fix chat.py file by adding initialize function
    - Enable link for chat in header and footer
    - Removed chat UI code from layouts and moved it into chat.html

--- Distributed Information Systems/chat.py
+++ Distributed Information Systems/chat.py
@@ -8,28 +8,15 @@ from nltk_utils import bag_of_words, tokenize
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-with open('intents.json', 'r') as json_data:
-    intents = json.load(json_data)
+intents = None
+tags = None
+model = None
+all_words = None
 
-FILE = "data.pth"
-data = torch.load(FILE)
-
-input_size = data["input_size"]
-hidden_size = data["hidden_size"]
-output_size = data["output_size"]
-all_words = data['all_words']
-tags = data['tags']
-model_state = data["model_state"]
-
-model = NeuralNet(input_size, hidden_size, output_size).to(device)
-model.load_state_dict(model_state)
-model.eval()
-
-bot_name = "Ada"
 
 def get_response(msg):
-    sentence = tokenize(msg)
-    X = bag_of_words(sentence, all_words)
+    sentences = tokenize(msg)
+    X = bag_of_words(sentences, all_words)
     X = X.reshape(1, X.shape[0])
     X = torch.from_numpy(X).to(device)
 
@@ -44,12 +31,35 @@ def get_response(msg):
         for intent in intents['intents']:
             if tag == intent["tag"]:
                 return random.choice(intent['responses'])
-    
+
     return "I do not understand..."
+
+
+def initialize(data_path, intents_path):
+    global intents, tags, model, all_words
+
+    with open(intents_path, 'r') as json_data:
+        intents = json.load(json_data)
+
+    data = torch.load(data_path)
+
+    input_size = data["input_size"]
+    hidden_size = data["hidden_size"]
+    output_size = data["output_size"]
+    all_words = data['all_words']
+    tags = data['tags']
+    model_state = data["model_state"]
+
+    model = NeuralNet(input_size, hidden_size, output_size).to(device)
+    model.load_state_dict(model_state)
+    model.eval()
+
+    bot_name = "Ada"
 
 
 if __name__ == "__main__":
     print("Let's chat! (type 'quit' to exit)")
+    initialize()
     while True:
         # sentence = "do you use credit cards?"
         sentence = input("You: ")
@@ -58,4 +68,3 @@ if __name__ == "__main__":
 
         resp = get_response(sentence)
         print(resp)
-
